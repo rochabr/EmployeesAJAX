9e56de832b9b8c3aacd5699c508d2fc64ec2b446
Fernando Rocha <fernandorsl@gmail.com>
2015-08-21 00:55:36 -0500

    Initial commit

---
+++ js/widget.js
@@ -0,0 +1,25 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4){
+        var employees = JSON.parse(xhr.responseText);
+        var statusHTML = '<ul class="bulleted">';
+
+        for (var i = 0; i < employees.length; i++){
+            if (employees[i].inoffice === true){
+                statusHTML += '<li class="in">'
+            }else{
+                statusHTML += '<li class="out">'
+            }
+
+            statusHTML += employees[i].name;
+            statusHTML += "</li>";
+        }
+        statusHTML += "</ul>";
+        console.log(statusHTML);
+
+        $("#employeeList").append(statusHTML);
+    }
+};
+
+xhr.open('GET', 'data/employees.json');
+xhr.send();
